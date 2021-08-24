46f42d6f65bb20bea1f6e6e63769430d1caad4bb
haiix <haiix268@gmail.com>
2021-08-24 19:33:04 +0900

    initApp追加

---
+++ assets/initApp.mjs
@@ -0,0 +1,31 @@
+export default async function initApp (App) {
+  let app = null
+  try {
+    app = new App()
+    document.body.appendChild(app.element)
+    window.app = app
+    if (app.main) await app.main()
+    if (app.loop) {
+      ;(async function loop (t) {
+        try {
+          await app.loop(t)
+        } catch (error) {
+          if (app.onerror) {
+            await app.onerror(error)
+          } else {
+            throw error
+          }
+        }
+        window.requestAnimationFrame(loop)
+      }(0))
+    }
+  } catch (error) {
+    if (app && app.onerror) {
+      app.onerror(error)
+    } else if (App.prototype.onerror) {
+      App.prototype.onerror.call(null, error)
+    } else {
+      throw error
+    }
+  }
+}
